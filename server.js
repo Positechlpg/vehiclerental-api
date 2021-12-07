759fe6f12993f02f0b83e8b29dd4a28cb417fdd1
Trisumanzaya93 <trisumanzaya93a@gmail.com>
2021-12-07 09:57:24 +0700

    presentasi

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const server = express()
 const routers = require('./src/routers')
+const morgan = require('morgan')
+const logger = morgan('combined') 
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+server.use(logger)
 server.use("/api", routers)
 module.exports = server
