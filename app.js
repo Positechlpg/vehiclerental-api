4bb17298d78a23f5bf514b2f0d3d873f1e4bf450
Trisumanzaya93 <trisumanzaya93a@gmail.com>
2022-01-20 13:42:47 +0700

    coba

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const server = require("./server")
 
-const port = 8000;
+
+const port = process.env.PORT || 8000;
 server.listen(port, () => {
   console.log(`server is running port ${port}`);
 })
