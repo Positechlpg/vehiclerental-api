b10ae3bf02a82b45ec502a01148b8c4e4f0a94b0
Trisumanzaya93 <trisumanzaya93a@gmail.com>
2022-03-25 09:25:02 +0700

    fixing delete

--- src/routers/history.js
+++ src/routers/history.js
@@ -2,14 +2,13 @@ const express = require('express')
 const historyRouter = express.Router()
 const historyController = require('./../controllers/historyControlers')
 const {checkToken} = require('../middleware/authentication')
-const {checkRoleAdmin} = require('../middleware/authorization')
 
 historyRouter
         .get('/',checkToken, historyController.getAllHistory)
         .get('/historyuser/:userid',checkToken, historyController.getHistoryByUserId)
         .post('/createhistory',checkToken,historyController.createHistory)
         .put('/:historyId',checkToken,historyController.updateHistory)
-        .delete('/:historyId',checkToken, checkRoleAdmin, historyController.deleteHistory)
+        .delete('/:historyId',checkToken, historyController.deleteHistory)
         .get('/vehiclelocation/:location', historyController.getPopulerVhicle)
 
 module.exports = historyRouter
