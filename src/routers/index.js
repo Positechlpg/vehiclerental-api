c151eeae5873392ff203c28c6ffbc81c9fb882a8
fadhlillahrizky <rizky.fadhlillah3@gmail.com>
2022-07-23 10:04:57 +0700

    clear all

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,15 +1,4 @@
 const express = require("express")
 const mainRouter = express.Router()
-const userRouter = require('./users.js')
-const vehicleRouter = require('./vehicle.js')
-const historyRouter = require('./history.js')
-const authRouter = require('./auth.js')
-const notifRouter = require("./notif.js")
-
-mainRouter.use('/users', userRouter)
-mainRouter.use('/vehicle',vehicleRouter)
-mainRouter.use('/history',historyRouter)
-mainRouter.use('/auth',authRouter)
-mainRouter.use('/notif',notifRouter)
 
 module.exports = mainRouter
